7e3f67bbcad88f2ad3ef8e6e684db848afd53f23
kaior <kaior@DESKTOP-L71BPU9>
2024-09-05 16:42:55 -0400

    Add project files.

---
+++ CSC 222 First Project.cpp
@@ -0,0 +1,10 @@
+// CSC 222 First Project.cpp : This is the starting point for two different programming projects.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "CSC 222 First Project\n";
+}
+
